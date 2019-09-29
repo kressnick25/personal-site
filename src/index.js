70ebb93b4f5705674abd0920284da6ae23c00c30
nkress <kressnick25@gmail.com>
2019-09-29 18:10:08 +1000

    .

--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import App from './App';
+import ReactDOM from 'react-dom'
 import * as serviceWorker from './serviceWorker';
-import { hydrate, render} from 'react-dom';
 
-const rootElement = document.getElementById("root");
+ReactDOM.render( <App/>, document.getElementById('root') );
 
-if (rootElement.hasChildNodes()){
-    hydrate(<App />, rootElement);
-} else {
-    render( <App />, rootElement);
-}
 
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
