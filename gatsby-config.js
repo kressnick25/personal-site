bb12895492b0440f366f4b3453f78372e08fd917
Nick Kress <kressnick25@gmail.com>
2021-03-21 11:51:56 +1000

    fix favico

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,11 +4,24 @@ module.exports = {
     siteUrl: `https://nkress.dev`,
     description: `Personal site of Nick Kress`,
   },
+  //pathPrefix: "/personal-site",
 //   proxy: {
 //     prefix: "/api",
 //     url: "http://examplesite.com/api/",
 //   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    {
+        resolve: `gatsby-plugin-manifest`,
+        options: {
+          name: 'Nick Kress Developer',
+          short_name: 'nkress',
+          start_url: '/',
+          background_color: '#f7f0eb',
+          theme_color: '#a2466c',
+          display: 'standalone',
+          icon: './src/images/logo192.png',      
+        }
+    },
   ]
 };
